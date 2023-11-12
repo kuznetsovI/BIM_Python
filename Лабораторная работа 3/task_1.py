3fcfd5260c243942caa42c278d335f11ad9f4f3a
kuznetsovI <149502223+kuznetsovI@users.noreply.github.com>
2023-11-12 22:58:24 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,14 @@
+def chek (spisok, fruit):
+    for poisk in spisok:
+        if poisk in fruit:
+            return spisok.index(fruit)
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = chek(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
